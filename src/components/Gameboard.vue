6426a0509c2291681a1986f05933add0ce18b23c
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-09-18 15:33:07 +0800

    basic display of gameboard

---
+++ src/components/Gameboard.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+export const SIZE = 15;
+</script>
+
+<script setup lang="ts">
+import PieceCell from "./PieceCell.vue";
+</script>
+
+<template>
+  <div
+    class="
+      gameboard
+      bg-yellow-200
+      rounded
+      shadow-md
+      border-4 border-gray-500
+      grid
+      p-1
+    "
+  >
+    <template v-for="y in SIZE" :key="y">
+      <PieceCell v-for="x in SIZE" :key="x" :x="x" :y="y" />
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.gameboard {
+  $margin-to-screen: 1.5rem;
+  $side-length: calc(100vmin - $margin-to-screen);
+  width: $side-length;
+  height: $side-length;
+
+  $grid-template: repeat(v-bind(SIZE), minmax(0, 1fr));
+  grid-template-columns: $grid-template;
+  grid-template-rows: $grid-template;
+}
+</style>
---
+++ src/components/PieceCell.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { SIZE as BOARD_SIZE } from "./Gameboard.vue";
+defineProps<{ x: number; y: number }>();
+</script>
+
+<template>
+  <div class="bg-white bg-opacity-0 hover:bg-opacity-50 text-gray-600">
+    <svg class="w-full h-full cell-line stroke-1">
+      <line
+        x1="50%"
+        :y1="y > 1 ? '0%' : '50%'"
+        x2="50%"
+        :y2="y < BOARD_SIZE ? '100%' : '50%'"
+        class="stroke-current"
+      />
+      <line
+        :x1="x > 1 ? '0%' : '50%'"
+        y1="50%"
+        :x2="x < BOARD_SIZE ? '100%' : '50%'"
+        y2="50%"
+        class="stroke-current"
+      />
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.cell-line {
+  stroke-linecap: round;
+}
+</style>
