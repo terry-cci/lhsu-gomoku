e217f3c41462732ebfad6f523cd76a0cb3198bab
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-09-26 00:01:30 +0800

    better pwa

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+var cacheName = "lhsu-gomoku";
+var filesToCache = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
