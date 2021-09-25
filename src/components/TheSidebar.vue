94cc7492a026de15d527e503d24b956e64ae15b5
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-09-25 18:12:56 +0800

    disabled history button when no history

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -164,8 +164,10 @@ function onOverlayClicked() {
             flex
             items-center
             justify-center
+            disabled:opacity-50
           "
           @click="replayModalOpen = !replayModalOpen"
+          :disabled="!placementHistory.length"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
