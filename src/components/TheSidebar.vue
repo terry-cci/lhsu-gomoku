fff53d7ef7ba3ad9da0893709105ea241abdebb1
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-12-20 19:59:35 +0800

    restart text adjustment

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -80,7 +80,7 @@ const replayModalOpen = ref(false);
       @cancel="restartModalOpen = false"
       @confirm="restartGame"
     >
-      確定要開始新的一局嗎?
+      確定要清除所有的數據嗎?
     </the-modal>
   </template>
 
@@ -229,7 +229,7 @@ const replayModalOpen = ref(false);
               d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
             />
           </svg>
-          重新開局
+          清除數據
         </button>
       </div>
     </transition>
