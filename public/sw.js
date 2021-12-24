2874b2f826f54a34074046d2e28494c799737f9e
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-12-24 13:20:04 +0800

    update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,30 @@
-var cacheName = "lhsu-gomoku";
-var filesToCache = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", function (e) {
+const version = "v3";
+const cacheName = `lhsu-gomoku-${version}`;
+
+const filesToCache = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
 });
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
